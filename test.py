096420ca43c935f19e63dcd780b92f21491e6ae2
Sharad117 <118285093+Sharad117@users.noreply.github.com>
2024-01-21 23:41:28 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import speech_2_text
-# Example usage:
-# converter = Conversion()
-# converter.speech('path/to/your/audio/file.wav')
-instance1= speech_2_text.Conversion()
-label,conf =instance1.speech('sample2.mp3')
-
-print(label, conf)
